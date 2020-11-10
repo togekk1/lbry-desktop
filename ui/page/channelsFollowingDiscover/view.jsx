8ca1e9ce3c7cb4b440e7bc35866072a87be713b5
Sean Yesmunt <sean@lbry.io>
2020-11-10 13:28:57 -0500

    fix rebase

--- ui/page/channelsFollowingDiscover/view.jsx
+++ ui/page/channelsFollowingDiscover/view.jsx
@@ -115,11 +115,9 @@ function ChannelsFollowingDiscover(props: Props) {
             {help}
           </h1>
 
-		<ClaimTilesDiscover {...options} />
+          <ClaimTilesDiscover {...options} />
         </div>
       ))} */}
-
-      {/* odysee: claimIds = PRIMARY_CONTENT_CHANNEL_IDS if simplesite CLD */}
 
       <ClaimListDiscover
         defaultOrderBy={CS.ORDER_BY_TRENDING}
